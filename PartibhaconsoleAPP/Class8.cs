ae172653605096c7f60fe2d1b08217cb05205693
Sachin-gif-05 <sachindahiyasirsa@gmail.com>
2026-04-09 14:24:28 +0530

    function class added

--- PartibhaconsoleAPP/Class8.cs
+++ PartibhaconsoleAPP/Class8.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace PartibhaconsoleAPP
-{
-    internal class Fucntion
-    {
-        static void Main()
-            //only a static function can call another static function directly without creating an object of the class
-        {
-             msg();
-            table(5);
-            factorial(5);
-            Console.WriteLine("Factorial of 5 is " + factorial(5));
-            Console.WriteLine("Value of pi is " + getpi());
+//namespace PartibhaconsoleAPP
+//{
+//    internal class Fucntion
+//    {
+//        static void Main()
+//            //only a static function can call another static function directly without creating an object of the class
+//        {
+//             msg();
+//            table(5);
+//            factorial(5);
+//            Console.WriteLine("Factorial of 5 is " + factorial(5));
+//            Console.WriteLine("Value of pi is " + getpi());
             
              
-        }
+//        }
 
-        static void msg()//function without return type and without parameter
-        {
-            Console.WriteLine("Hello World");
-        }
+//        static void msg()//function without return type and without parameter
+//        {
+//            Console.WriteLine("Hello World");
+//        }
 
-        static void table(int x)// function without return type but accepting a value
+//        static void table(int x)// function without return type but accepting a value
           
-        {
-            int i;
-            for( i = 1; i < 11; i++)
-            {
-                Console.WriteLine($"{x} x{i}={x * i}");
-            }
-        }
+//        {
+//            int i;
+//            for( i = 1; i < 11; i++)
+//            {
+//                Console.WriteLine($"{x} x{i}={x * i}");
+//            }
+//        }
 
-        static int factorial(int x)
-        { //function with return type and accepting a value
+//        static int factorial(int x)
+//        { //function with return type and accepting a value
 
-            if (x == 0){
-                return 1;
-            }
-            else
-            {
-                return factorial(x - 1) * x;
-            }
-        }
-        static double getpi() //This function doesnot accept any value but return a value
-        {
-            return 3.14;
-        }
-    }
-}
+//            if (x == 0){
+//                return 1;
+//            }
+//            else
+//            {
+//                return factorial(x - 1) * x;
+//            }
+//        }
+//        static double getpi() //This function doesnot accept any value but return a value
+//        {
+//            return 3.14;
+//        }
+//    }
+//}
---
+++ PartibhaconsoleAPP/Class9.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace PartibhaconsoleAPP
+//{
+//    internal class Function
+//    {
+
+//        static void Main()
+//        {
+//            int a = 7, b = 8;
+//            Console.WriteLine($"Values before swapping: a={a} and b={b}");
+//            Swap(a, b);
+//            Console.WriteLine($"Values after swapping: a={a} and b={b}");
+
+//            Swapref(ref a, ref b); //c# don't have pointers and call by reference is achieved by using ref keyword
+//            Console.WriteLine($"value after swapping call by reference: a={a} and b={b}");
+//            int hindi = 20, english = 30, math = 49;
+//            int sum, average; 
+//            Result(hindi,english,math,out sum,out average);//out type is new in c#  . it is combination of call by refernce and call by value
+//            Console.WriteLine($"sum={sum} and average={average}");
+
+//        }
+
+
+
+//        static void Swap(int a, int b)//call by value
+//        {
+//            int temp = a;
+//            a = b;
+
+//            b = temp;
+//        }
+
+
+
+//        static void Swapref(ref int a, ref int b)//call by reference
+//        {
+//            int temp = a;
+//            a = b;
+//            b = temp;
+//        }
+
+//        static void Result(int hindi, int english, int math, out int sum, out int average)
+//        {
+//            sum = hindi + english + math;
+//            average = sum / 3;
+//        }
+//    }
+//}
